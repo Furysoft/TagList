d1cc194c527ae3297a4aa627dd93ee3202bd97a8
Simon Paramore <simon.paramore@emailhippo.com>
2020-02-13 00:01:25 +0000

    Set up project

---
+++ src/Component/Furysoft.TagList/TagList.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TagList.cs" company="Simon Paramore">
+// © 2020, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.TagList
+{
+    using System;
+
+    /// <summary>
+    /// The Tag List.
+    /// </summary>
+    public sealed class TagList
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagList" /> class.
+        /// </summary>
+        /// <param name="tagListType">Type of the tag list.</param>
+        public TagList(TagListType tagListType)
+        {
+            this.TagListType = tagListType;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TagList" /> class.
+        /// </summary>
+        /// <param name="tagListType">Type of the tag list.</param>
+        public TagList(string tagListType)
+        {
+            this.TagListType = (TagListType)Enum.Parse(typeof(TagListType), tagListType);
+        }
+
+        /// <summary>Gets the type of the tag list.</summary>
+        public TagListType TagListType { get; }
+    }
+}
---
+++ src/Component/Furysoft.TagList/TagListType.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TagListType.cs" company="Simon Paramore">
+// © 2020, Simon Paramore
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Furysoft.TagList
+{
+    /// <summary>
+    /// The Tag List Type.
+    /// </summary>
+    public enum TagListType
+    {
+        /// <summary>
+        /// The and.
+        /// </summary>
+        And,
+
+        /// <summary>
+        /// The or.
+        /// </summary>
+        Or,
+    }
+}
